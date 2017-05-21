a96a889148f3d352872a3fea1017597b4362f314
cector-huang <huangjiaqi@sc.10086.cn>
2017-05-21 23:09:59 +0800

    “添加数据访问对象DTO”

---
+++ src/main/java/com/cector/mysqldao/MysqlDto.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cector.mysqldao;
+
+import java.util.Date;
+
+/**
+ *
+ * @author huangjiaqi
+ */
+public class MysqlDto {
+    private int id;
+    private String name;
+    private String address;
+    private Date time;
+    
+    public int getId()
+    {
+        return id;
+    }
+    
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
+    
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    
+    public String getAddress()
+    {
+        return address;
+    }
+    
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
+    
+    public Date getTime()
+    {
+        return time;
+    }
+    
+    public void setTime()
+    {
+        this.time = time;
+    }
+    
+    public MysqlDto(int id, String name, String address, Date time)
+    {
+        super();
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.time = time;
+    }
+    
+    public MysqlDto()
+    {
+        super();
+    }
+    
+    public MysqlDto(String name, String address, Date time)
+    {
+        super();
+        this.name = name;
+        this.address = address;
+        this.time = time;
+    }
+}
